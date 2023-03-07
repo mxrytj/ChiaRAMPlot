0fd4ddf97ef1c77af59b9b35d0e82fbe9eba9d96
mxrytj <86047703+mxrytj@users.noreply.github.com>
2023-03-07 17:54:27 +0800

    Update endianness.h

--- uint128_t/endianness.h
+++ uint128_t/endianness.h
@@ -1,18 +1,14 @@
 #ifndef _ENDIANNESS_H_
 #define _ENDIANNESS_H_
 #if defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN || \
-    defined(__BIG_ENDIAN__) ||                               \
-    defined(__ARMEB__) ||                                    \
-    defined(__THUMBEB__) ||                                  \
+    defined(__BIG_ENDIAN__) ||                               \                             \
     defined(__AARCH64EB__) ||                                \
     defined(_MIBSEB) || defined(__MIBSEB) || defined(__MIBSEB__)
 #ifndef __BIG_ENDIAN__
 #define __BIG_ENDIAN__
 #endif
 #elif defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN || \
-    defined(__LITTLE_ENDIAN__) ||                                 \
-    defined(__ARMEL__) ||                                         \
-    defined(__THUMBEL__) ||                                       \
+    defined(__LITTLE_ENDIAN__) ||                                 \                                      \
     defined(__AARCH64EL__) ||                                     \
     defined(_MIPSEL) || defined(__MIPSEL) || defined(__MIPSEL__) || \
     defined(_WIN32) || defined(__i386__) || defined(__x86_64__) || \
